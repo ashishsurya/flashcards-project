b0751f1059da226dbafaa70dee2f6d6e91ea2a7f
Surya Ashish <ashishsurya126@gmail.com>
2023-05-13 00:06:28 +0530

    login page completed

--- web-frontend/src/components/common/ProtectedRoute.tsx
+++ web-frontend/src/components/common/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export const ProtectedRoute = (props: any) => {
+export const ProtectedRoute = (props: { children: JSX.Element }) => {
   const navigate = useNavigate();
   useEffect(() => {
     const token = localStorage.getItem('token');
--- web-frontend/src/components/screens/LoginScreen.tsx
+++ web-frontend/src/components/screens/LoginScreen.tsx
@@ -1,11 +1,33 @@
+import { Input } from '../common/Input';
+import logo from '../../../public/logo.svg';
 export const LoginScreen = () => {
   return (
-    <div className='grid place-items-center h-screen'>
-      <div className='p-8 border'>
-        <h1 className='text-3xl font-bold'>Sign in to, memoizeeeee 🧠🧠</h1>
+    <div className='grid place-items-center h-full md:h-[80vh]'>
+      <div className='p-8  border-slate-600 rounded-md flex flex-col space-y-7'>
+        <img src={logo} alt='' className='w-[300px] mx-auto mb-4' />
+        <h1 className='text-3xl md:text-5xl font-mono text-[#929292]'>
+          A minimalist flashcards app 
+        </h1>
+        <form className='flex flex-col space-y-5 items-stretch w-3/5 mx-auto'>
+          <Input
+            placeholder='enter your email address'
+            type='email'
+            label='Email'
+            fullWidth
+            required
+          />
+          <Input
+            placeholder='enter your password'
+            type='password'
+            label='Password'
+            fullWidth
+            required
+          />
+          <button className='bg-slate-200 text-slate-800 px-4 py-2 tracking-tighter font-bold hover:bg-opacity-90'>
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
 };
-
-
