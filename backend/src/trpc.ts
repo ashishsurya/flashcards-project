b7d029bfed99e8603729ab2232baaae376572211
Surya Ashish <ashishsurya126@gmail.com>
2023-05-13 15:55:24 +0530

    zod error formatter added

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,6 +1,7 @@
 import { inferAsyncReturnType, initTRPC } from '@trpc/server';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { prisma } from './prisma';
+import { ZodError } from 'zod';
 
 export const createContext = ({
   req,
@@ -10,7 +11,18 @@ export const createContext = ({
   req,
 }); // no context
 
-export const t = initTRPC.context<typeof createContext>().create();
+export const t = initTRPC.context<typeof createContext>().create({
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
 export const publicProcedure = t.procedure;
 export const router = t.router;
 export const middleware = t.middleware;
@@ -18,5 +30,4 @@ export const middleware = t.middleware;
 // const isAuthenticated = middleware(async (opts) => {
 //   // TODO : check if user is authenticated and return error if not
 
-  
 // });
