59c06d8feeb05882bf2f20863b782d81383aaeff
Surya Ashish <ashishsurya126@gmail.com>
2023-05-19 18:03:13 +0530

    vite to nextjs

--- react/src/components/display/DeckView.tsx
+++
@@ -1,9 +0,0 @@
-export const DeckView = () => { 
-
-  return (
-    <div className="">
-      {/* left button  &  current flashcard  &  right button */}
-      {/* all flashcards */}
-    </div>
-  )
-}
